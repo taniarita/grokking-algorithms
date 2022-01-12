0cd2d2163e64e5fe8faeb63b1f964edf050dd66b
Tania Rita <taniamendes6523@gmail.com>
2022-01-12 16:09:40 -0300

    adding comments

--- binarySearch.js
+++ binarySearch.js
@@ -1,3 +1,5 @@
+// Capítulo 1 - Introdução a algoritmos / Notação Big O
+
 function binarySearch(list, item) {
   let low = 0;
   let high = list.length - 1;
@@ -21,4 +23,15 @@ const listTest = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const test = binarySearch(listTest, 5);
 
-console.log(test);
+console.log(test); 
+
+/*  Com a pesquisa binária, você chuta um número intermediário e elimina a metade dos números restantes a cada vez;
+    Log n na base 2;
+    Não basta saber quanto tempo um algoritmo leva para ser executado, é preciso saber se o tempo de execução aumenta conforme a lista aumenta;
+ */
+
+// A pesquisa binária é muito mais rápida que a pesquisa simples;
+// A rapidez de um algoritmo não é medida em segundos;
+// O(log n) é mais rápido que O(n), e O(log n) fica ainda mais raṕido conforme os elemntos da lista aumentam;
+// O tempo de execução de um algoritmo é medido por meio de seu crescimento;
+// O tempo de execução dos algoritmos é expresso na notação Big O;
